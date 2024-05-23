923b932c9f70cc29da264416007c72aa4ea96d01
hkl0217 <167002591+hkl0217@users.noreply.github.com>
2024-05-23 08:28:49 -0400

    Update moments.js

--- moments.js
+++ moments.js
@@ -1,7 +1,9 @@
-function selectPhilippines () {
-let philippinesTime = moment ()
-.tz("Asia/Manila")
-.format ("dddd, MMMM Do YYYY hh:mm a");
-}
-
-alert (`It is ${philippinesTime} in Philippines`);
+function chosenCounty(event) {
+    let chosenCountry = event.target.value;
+    let selectedCountry = moment()
+      .tz(`${chosenCountry}`)
+      .format("dddd, MMMM DD YYYY hh:mm a");
+    alert(`It is ${selectedCountry} in ${chosenCountry}`);
+  }
+  let countries = document.querySelector("#selected-countries");
+  countries.addEventListener("change", chosenCountry);
