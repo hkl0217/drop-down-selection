19c9373d39c4241b873b318184215d0540008f63
hkl0217 <167002591+hkl0217@users.noreply.github.com>
2024-05-23 08:17:01 -0400

    JS
    testing JS

--- moments.js
+++ moments.js
@@ -2,4 +2,4 @@ let philippinesTime = moment ()
 .tz("Asia/Manila")
 .format ("dddd, MMMM Do YYYY hh:mm a");
 
-alert (`It is ${philippinesTime} in Philippines`)
+alert (`It is ${philippinesTime} in Philippines`);
