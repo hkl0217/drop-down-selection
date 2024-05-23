a1f843f949d75ad9e210582aa320cc965a1c52cb
hkl0217 <167002591+hkl0217@users.noreply.github.com>
2024-05-23 08:20:42 -0400

    Update moments.js

--- moments.js
+++ moments.js
@@ -1,5 +1,7 @@
+function selectPhilippines () {
 let philippinesTime = moment ()
 .tz("Asia/Manila")
 .format ("dddd, MMMM Do YYYY hh:mm a");
+}
 
 alert (`It is ${philippinesTime} in Philippines`);
